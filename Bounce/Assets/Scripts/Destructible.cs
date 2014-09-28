c784c730c4c3f648a2d5d843107260edbf097f81
SkirkR <S.Kirk.R@gmail.com>
2014-09-27 22:13:27 -0400

    Added destructible option

---
+++ Bounce/Assets/Scripts/Destructible.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destructible : MonoBehaviour {
+	// Threshold velocity value
+	public float vThres = 5f;
+	// Use this for initialization
+	void OnCollisionEnter2D (Collision2D col){
+		// Must be player
+		if (col.gameObject.tag == "Player") {
+			// Must match the necessary speed
+						if (col.gameObject.rigidbody2D.velocity.magnitude >= vThres) {
+				//Destroys the object
+								Destroy (gameObject);
+						}
+				}
+	}
+}
