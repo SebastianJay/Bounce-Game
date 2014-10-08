ecfbd4febb00a35566bd2f91bd004e9981557802
dot4qu <dot4qu@virginia.edu>
2014-10-08 15:15:25 -0400

    Added moving platform basics
    can add moving platform script to an object and set its initial X and Y
    velocities. moves forever for now

---
+++ Bounce/Assets/Scripts/Environment/Moving_Platform.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moving_Platform : MonoBehaviour {
+	
+	bool way = false;  //platform direction
+	public int platformSpeedHorizontal;
+	public int platformSpeedVertical;
+
+	void Start() {
+		//initializes platform with velocities entered by user
+		rigidbody2D.velocity = new Vector2(platformSpeedHorizontal, platformSpeedVertical);
+		}
+
+
+
+	void Update () {
+
+	}
+}
