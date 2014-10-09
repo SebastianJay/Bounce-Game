4d81667e158b3494234c72b98fe636b25a7e51c5
SebastianJay <sebastianjay2@gmail.com>
2014-10-09 15:42:27 -0400

    Prelim city
    A mockup of the house portion of the city level

--- Bounce/Assets/Scripts/Player/PlayerDataManager.cs
+++ Bounce/Assets/Scripts/Player/PlayerDataManager.cs
@@ -6,6 +6,7 @@ public class PlayerDataManager : MonoBehaviour {
 	// Use this for initialization
 	public int lastCheckpoint = 0;
 	public int lastLevel = 0;
+	public bool debugNoLoad = false;
 	public Dictionary<int,List<int>> previousCheckpoints = new Dictionary<int,List<int>>();
 	public Inventory inventory = new Inventory();
 	public HashSet<string> gameConstants = new HashSet<string>();
@@ -15,7 +16,7 @@ public class PlayerDataManager : MonoBehaviour {
 
 	void Start () {
 		playerData myData = XmlSerialzer.Load ();
-		if(myData != null && !loadedLevel)
+		if(myData != null && !loadedLevel && !debugNoLoad)
 		{
 			lastLevel = myData.lastLevel;
 			initialLevel = lastLevel;
