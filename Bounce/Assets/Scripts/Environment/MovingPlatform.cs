b8bbe9c22d68d772dc333dc49fa007e129e97e80
SebastianJay <sebastianjay2@gmail.com>
2014-10-17 16:37:36 -0400

    Tweaked moving platform
    May bounce or move player while moving - something to possibly fix if we
    use it later

--- Bounce/Assets/Scripts/Environment/MovingPlatform.cs
+++ Bounce/Assets/Scripts/Environment/MovingPlatform.cs
@@ -1,22 +1,68 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Moving_Platform : MonoBehaviour {
+public class MovingPlatform : MonoBehaviour {
+	public Vector2 initialPoint;
+	public Vector2 endPoint;
+	public bool useCurrentStartPosition = false;
+	public float moveTime = 5.0f;	//in seconds, from start to end
+	public float pauseTime = 2.0f;	//in seconds, time at endpoints
+
+	private float moveTimer = 0.0f;
+	private float pauseTimer = 0.0f;
+	private bool paused = false;
+	private Vector2 mFrom;
+	private Vector2 mTo;
+
+	void Start() {
+		if (useCurrentStartPosition)
+			initialPoint = rigidbody2D.position;
+		else
+			transform.position = new Vector3(initialPoint.x, initialPoint.y, 0);
+		mFrom = initialPoint;
+		mTo = endPoint;
+	}
+
+	void Update () {
+		if (paused)
+		{
+			pauseTimer += Time.deltaTime;
+			if (pauseTimer >= pauseTime)
+			{
+				pauseTimer = 0.0f;
+				paused = false;
+			}
+		}
+		else
+		{
+			moveTimer += Time.deltaTime;
+			if (moveTimer >= moveTime)
+			{
+				moveTimer = 0.0f;
+				Vector2 temp = mFrom;
+				mFrom = mTo;
+				mTo = temp;
+				paused = true;
+			}
+			float frac = moveTimer / moveTime;
+			rigidbody2D.MovePosition (Vector2.Lerp (mFrom, mTo, Mathf.Clamp (frac, 0.0f, 1.0f)));
+		}
+	}
+
+	///Alternate implementation
+	/*
 	public int platformSpeedHorizontal;
 	public int platformSpeedVertical;
 	private int distance = 5; //distance from initial point
 	private float initialX; //x position it starts at
 	private float initialY; //y position it starts at
-
 	void Start() {
+
 		//initializes platform with velocities entered by user
 		initialX = rigidbody2D.position.x;
 		initialY = rigidbody2D.position.y;
 		rigidbody2D.velocity = new Vector2(platformSpeedHorizontal, platformSpeedVertical);
-		}
-
-
-
+	}
 	void Update () {
 		//when the platform is moving right and reaches the specified distance, it turns around
 		if (rigidbody2D.position.x > initialX &&(rigidbody2D.position.x - initialX) >= distance) {
@@ -40,7 +86,6 @@ public class Moving_Platform : MonoBehaviour {
 			Vector2 reverseDirection = new Vector2(rigidbody2D.velocity.x, -rigidbody2D.velocity.y);
 			rigidbody2D.velocity = reverseDirection;
 		}
-
 	}
-
+	*/
 }
