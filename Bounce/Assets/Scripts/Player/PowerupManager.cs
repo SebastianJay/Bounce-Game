34e059c9c320c55af7a37a056b52bf79cec03d98
SebastianJay <sebastianjay2@gmail.com>
2014-11-03 11:44:44 -0500

    Cleaned up PowerupManager

--- Bounce/Assets/Scripts/Player/PowerupManager.cs
+++ Bounce/Assets/Scripts/Player/PowerupManager.cs
@@ -1,66 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//Attach this component to the player
 public class PowerupManager : MonoBehaviour {
 
 	public PowerupType currentPowerup = PowerupType.Normal;
 
-	public float superJumpBoost = 3500f;
+	public float superJumpBoost = 3500f;	//new jump force
 	public float superJumpTime = 10f;	//in seconds
 
-	private int jumpBoost = 5;
-	//Factor to increase jump height by
-
 	private GameObject player;
+	private float powerupTimer = 0f;
+	private float powerupTime = 1f;
 	private float normalJumpForce;
 	void Start()
 	{
-		player = GameObject.FindGameObjectWithTag ("Player");
+		//player = GameObject.FindGameObjectWithTag ("Player");
+		player = gameObject;
 		normalJumpForce = player.GetComponent<PlayerBallControl> ().jumpForce;
 	}
+
 	public void ActivatePowerup(PowerupType type)
 	{
+		if (currentPowerup != PowerupType.Normal)
+		{
+			EndPowerup();
+		}
+
 		currentPowerup = type;
 		PlayerBallControl pbc = player.GetComponent<PlayerBallControl> ();
-		switch (type)
+		switch (currentPowerup)
 		{
 		case PowerupType.SuperJump:
 			pbc.jumpForce = superJumpBoost;
-			StartCoroutine(EndOfPowerup(superJumpTime, 0));
+			powerupTime = superJumpTime;
+			//StartCoroutine(EndOfPowerup(superJumpTime));
 			break;
 		}
+		powerupTimer = 0f;
 	}
 
 	void Update () 
 	{
-		/*
-		PlayerBallControl pbc = GameObject.FindWithTag ("Player").GetComponent<PlayerBallControl>();
-		//Gets access to the control script
-		if (pbc.powerupState == PowerupType.SuperJump)
-			//Accesses if there's a current power up type assigned
+		if (currentPowerup != PowerupType.Normal)
 		{
-			pbc.jumpForce += jumpBoost;
-			//Adds the force factor to the control script
-			StartCoroutine(EndOfPowerup(10.0f, 0));
-			//Begins end of powerup coroutine, allows for waiting until powerup is finished, gives power up identifier, "0"
+			powerupTimer += Time.deltaTime;
+			if (powerupTimer >= powerupTime)
+			{
+				EndPowerup();
+			}
 		}
-		//If statement so that the script can be used with any powerup, add ifs as needed
-		*/
 	}
 
-	IEnumerator EndOfPowerup(float waitTime, int powerType) {
-		//Can be used with any powerup, accepts any time to end the powerup
+	public void EndPowerup()
+	{
 		PlayerBallControl pbc = player.GetComponent<PlayerBallControl>();
-		//Gets access to the control script
-		yield return new WaitForSeconds(waitTime);
-		//Script waits until time has passed, then ends the powerup
-		if (powerType == 0) 
+		switch(currentPowerup)
 		{
+		case PowerupType.SuperJump:
 			pbc.jumpForce = normalJumpForce;
-			//Sets player to no superJump
+			break;
 		}
-		//If statement so that the coroutine can be called with any powerup type, add more ifs as needed
-
 		currentPowerup = PowerupType.Normal;
 	}
 }
