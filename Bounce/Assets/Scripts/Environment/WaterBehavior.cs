075c55aa91358d6f6447bf945831e4781e7dff32
dot4qu <dot4qu@virginia.edu>
2014-11-13 19:21:26 -0500

    Updated water behavior script

--- Bounce/Assets/Scripts/Environment/WaterBehavior.cs
+++ Bounce/Assets/Scripts/Environment/WaterBehavior.cs
@@ -2,34 +2,39 @@
 using System.Collections;
 
 public class WaterBehavior : MonoBehaviour {
-	private Vector2 initialPlayerVector;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	private Vector2 waterForce;
+	private Vector2 entryVelocity;
+	private Vector2 dampingForce = new Vector2(0, -900);
 
-	void onTriggerEnter2D(Collider2D c) {
-		if (c.gameObject.tag == "ball") {
-						initialPlayerVector = new Vector2 (c.rigidbody.velocity.x, c.rigidbody.velocity.y);
-						Debug.Log ("entering");
+	void OnTriggerEnter2D(Collider2D c) {
+		entryVelocity = c.gameObject.rigidbody2D.velocity;
+		Debug.Log (c.gameObject.rigidbody2D.velocity.y);
+		if (-entryVelocity.y > 7) {
+						waterForce = new Vector2 (0, 450);
+						dampingForce = new Vector2 (0, -1200);
+				}
+				else if (entryVelocity.y > 5 && -entryVelocity.y < 7)
+						waterForce = new Vector2 (0, 300);
+				else if (-entryVelocity.y > 3 && -entryVelocity.y < 5)
+						waterForce = new Vector2 (0, 290);
+				//else if (-entryVelocity.y < 3) {
+				//		c.gameObject.rigidbody2D.velocity = new Vector2 (c.gameObject.rigidbody2D.velocity.x, 0);
+				//}
+				else
+						waterForce = new Vector2 (0, 450);
+		}
+
+	void OnTriggerStay2D(Collider2D c) {
+		if (c.gameObject.tag == "Player") {
+			//if (!(-entryVelocity.y > 3))
+			c.gameObject.rigidbody2D.AddForce(waterForce);
 				}
 		}
 
-	void onTriggerStay2D(Collider2D c) {
-		Vector2 forceVector = new Vector2 (0, -c.rigidbody.velocity.y);
-		c.rigidbody.AddForce (forceVector);
-		Debug.Log ("still inside");
+	void OnTriggerExit2D(Collider2D c) {
+		if (c.gameObject.tag == "Player") {
+			dampingForce = new Vector2(0, -900);
+			c.gameObject.rigidbody2D.AddForce(dampingForce);
+				}
 		}
-
-	void onCollisionEnter2D(Collision2D c) {
-		if (c.gameObject.tag == "ball") {
-			initialPlayerVector = new Vector2 (c.rigidbody.velocity.x, c.rigidbody.velocity.y);
-			Debug.Log ("entering");
-		}
-	}
 }
