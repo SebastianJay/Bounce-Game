03a1ef8ead7a6ed8607d5e15fa6c98c75a6a80c5
SebastianJay <sebastianjay2@gmail.com>
2014-11-15 16:32:19 -0500

    Added dialogue selection
    Very rudimentary at the moment

--- Bounce/Assets/Scripts/Dialogue/Interactable.cs
+++ Bounce/Assets/Scripts/Dialogue/Interactable.cs
@@ -6,47 +6,57 @@ public class Interactable : MonoBehaviour {
 	public TextAsset dialogueFile;
 	private Interaction dialogue;
 	public TextMesh gText;
-	public TextMesh hText;
+
 	private bool inTrigger = false;
+	private GameObject playerObj;
+	private bool inConveration = false;
 
 	// Use this for initialization
 	void Awake () {
 		dialogue = new Interaction (dialogueFile);
-		hText = GameObject.FindGameObjectWithTag ("HeroText").GetComponent<TextMesh> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown("Action") && inTrigger)
 		{
-			List<string> lines = dialogue.Step();
+			if (!inConveration)
+				inConveration = true;
+			List<string> lines = dialogue.Step(playerObj.GetComponent<DialogueResponses>().cursor);
 			if(lines.Count > 0)
 			{
 				gText.text = lines[0];
-				hText.text = "";
-				for (int i = 1; i < 2 && i < lines.Count; i++)	//this is temporary!!
-					hText.text += lines[i] + "\n";
+				List<string> heroLines = lines.GetRange(1, lines.Count - 1);
+				playerObj.GetComponent<DialogueResponses>().SetResponses(heroLines);
+				playerObj.rigidbody2D.velocity = Vector2.zero;
 			}
 			else
 			{
 				gText.text = "";
-				hText.text = "";
+				inConveration = false;
 			}
+		}
+		else if (Input.GetButtonDown("Jump") && inConveration)
+		{
+			playerObj.GetComponent<DialogueResponses>().MoveCursor(true);
+		}
+		else if (Input.GetButtonDown("Floor") && inConveration)
+		{
+			playerObj.GetComponent<DialogueResponses>().MoveCursor(false);
 		}
 	}
 	
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.tag == "Player") {
 			//animation appears to show you can talk
-
 			inTrigger = true;
+			playerObj = other.gameObject;
 		}
 	}
 	
 	void OnTriggerExit2D(Collider2D other) {
 		if (other.tag == "Player") {
 			//animation disappears
-
 			inTrigger = false;
 		}
 	}
---
+++ Bounce/Assets/Scripts/Player/DialogueResponses.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class DialogueResponses : MonoBehaviour {
+
+	public Transform textMeshPrefab;
+	public int cursor;
+
+	private List<Transform> textList 
+		= new List<Transform>();
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void SetResponses(List<string> responses) 
+	{
+		if (responses == null || responses.Count == 0)
+		{
+			DestroyResponses();
+			return;
+		}
+
+		cursor = 0;
+		int i = 0;
+		DestroyResponses();
+		foreach (string response in responses)
+		{
+			Transform obj = Instantiate(textMeshPrefab, transform.position, Quaternion.identity) as Transform;
+			obj.GetComponent<TextMesh>().text = response;
+			obj.transform.position = new Vector3(transform.position.x, 
+			                                     transform.position.y + 1.5f - i*0.4f,
+			                                     transform.position.z);
+			textList.Add(obj);
+			i++;
+		}
+		if (responses.Count > 1)
+			textList[0].GetComponent<TextMesh>().color = Color.yellow;	//current selection
+	}
+
+	public void DestroyResponses()
+	{
+		foreach (Transform trans in textList)
+			Destroy (trans.gameObject);
+		textList.Clear();
+	}
+
+	public void MoveCursor(bool up)
+	{
+		int numChoices = textList.Count;
+		if (numChoices <= 1)
+			return;
+		textList[cursor].GetComponent<TextMesh>().color = Color.black;
+		if (up)
+		{
+			cursor--;
+			if (cursor < 0)
+				cursor = numChoices - 1;
+		}
+		else //otherwise it's down!
+		{
+			cursor = (cursor + 1) % numChoices;
+		}
+		textList[cursor].GetComponent<TextMesh>().color = Color.yellow;
+	}
+}
