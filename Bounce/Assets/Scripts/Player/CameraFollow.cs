13b29e05f577d2a7b7dd9d5bd5f6fb23698ed793
SebastianJay <sebastianjay2@gmail.com>
2014-11-23 13:51:57 -0500

    Revised camera controls

--- Bounce/Assets/Scripts/Player/CameraFollow.cs
+++ Bounce/Assets/Scripts/Player/CameraFollow.cs
@@ -76,21 +76,33 @@ public class CameraFollow : MonoBehaviour
 		float targetY = transform.position.y;
 		float targetSize = cam.orthographicSize;
 
+		if (transform.position.x < minXAndY.x && player.position.x < minXAndY.x)
+			targetX = Mathf.Lerp(transform.position.x, minXAndY.x, xSmooth * Time.deltaTime);
+		else if (transform.position.x > maxXAndY.x && player.position.x > maxXAndY.x)
+			targetX = Mathf.Lerp(transform.position.x, maxXAndY.x, xSmooth * Time.deltaTime);
 		// If the player has moved beyond the x margin...
-		if(Mathf.Abs(transform.position.x - player.position.x) > xMargin)
+		else if(Mathf.Abs(transform.position.x - player.position.x) > xMargin)
+		{
 			// ... the target x coordinate should be a Lerp between the camera's current x position and the player's current x position.
 			targetX = Mathf.Lerp(transform.position.x, player.position.x, xSmooth * Time.deltaTime);
+		}
 
+		if (transform.position.y < minXAndY.y && player.position.y < minXAndY.y)
+			targetY = Mathf.Lerp(transform.position.y, minXAndY.y, ySmooth * Time.deltaTime);
+		else if (transform.position.y > maxXAndY.y && player.position.y > maxXAndY.y)
+			targetY = Mathf.Lerp(transform.position.y, maxXAndY.y, ySmooth * Time.deltaTime);
 		// If the player has moved beyond the y margin...
-		if(Mathf.Abs(transform.position.y - player.position.y) > yMargin)
+		else if(Mathf.Abs(transform.position.y - player.position.y) > yMargin)
+		{
 			// ... the target y coordinate should be a Lerp between the camera's current y position and the player's current y position.
 			targetY = Mathf.Lerp(transform.position.y, player.position.y, ySmooth * Time.deltaTime);
+		}
 
 		targetSize = Mathf.Lerp(cam.orthographicSize, lockedOrthoSize, Time.deltaTime * orthoSmooth);
 
 		// The target x and y coordinates should not be larger than the maximum or smaller than the minimum.
-		targetX = Mathf.Clamp(targetX, minXAndY.x, maxXAndY.x);
-		targetY = Mathf.Clamp(targetY, minXAndY.y, maxXAndY.y);
+		//targetX = Mathf.Clamp(targetX, minXAndY.x, maxXAndY.x);
+		//targetY = Mathf.Clamp(targetY, minXAndY.y, maxXAndY.y);
 
 		// Set the camera's position to the target position with the same z component.
 		transform.position = new Vector3(targetX, targetY, transform.position.z);
