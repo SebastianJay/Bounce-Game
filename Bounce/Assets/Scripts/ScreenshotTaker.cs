5a167b7973c30621066e83c0995a85c3a6a73559
SebastianJay <sebastianjay2@gmail.com>
2015-02-15 01:49:37 -0500

    made screenshot app
    + minor changes in city

---
+++ Bounce/Assets/Scripts/ScreenshotTaker.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class ScreenshotTaker : MonoBehaviour {
+
+	public string path = "../Concept_Dropbox/Screenshots/";
+	public string prefix = "Bounce_screen_";
+	public string suffix = ".png";
+	public int supersize = 0;
+
+	private int index;
+
+	void Start() {
+		string[] files = Directory.GetFiles(path, prefix + "*" + suffix);
+		index = files.Length;
+	}
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.P))
+		{
+			Application.CaptureScreenshot(path + prefix + index.ToString() + suffix, supersize);
+			index++;
+		}
+	}
+}
