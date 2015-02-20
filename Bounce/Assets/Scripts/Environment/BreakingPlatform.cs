5c753ed5c03bf633c6d69330acaf5b88f522e458
cahruhr <cahruhr2795@gmail.com>
2015-02-20 14:55:55 -0500

    Added breakable platforms

---
+++ Bounce/Assets/Scripts/Environment/BreakingPlatform.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakingPlatform : MonoBehaviour {
+
+	public int timer = 20;
+	private bool timerStarted = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Update () {
+		if (this.timerStarted && this.timer > 0) {
+			this.timer--;
+		}
+	}
+
+
+	void OnCollisionEnter2D(Collision2D coll) 
+	{
+		if (coll.gameObject.tag == "Player") 
+		{
+			this.timerStarted = true;
+		}
+		if (this.timer <= 0) {
+			this.gameObject.rigidbody2D.fixedAngle = false;
+		}
+	}
+}
