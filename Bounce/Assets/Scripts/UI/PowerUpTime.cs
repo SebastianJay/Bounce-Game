564bc0ebf473514b8d35a346903431124e19a652
Joseph Baik <josephsbaik@gmail.com>
2015-04-02 14:58:54 -0400

    powerup is now a bar

--- Bounce/Assets/Scripts/UI/PowerUpTime.cs
+++ Bounce/Assets/Scripts/UI/PowerUpTime.cs
@@ -12,6 +12,14 @@ public class PowerUpTime : MonoBehaviour {
 	public Texture2D fgImage;
 	public float timerBarLength;
 	
+	float barDisplay = 0; 
+	Vector2 pos = new Vector2(20,40);
+	Vector2 size = new Vector2(60,20);
+//	public Texture2D progressBarEmpty;
+//	public Texture2D progressBarFull;
+	public GUITexture pbbackground;
+	public GUITexture powerbar;
+	
 	void Start()
 	{
 		player = GameObject.FindGameObjectWithTag ("Player");
@@ -31,21 +39,40 @@ public class PowerUpTime : MonoBehaviour {
 	}
 	
 	void OnGUI(){
-		GUI.Label (new Rect (735, 12, 60, 20), timetext + timer.ToString(), style);
-		//GUI.Box (new Rect (10, 10, 0.001 * Screen.width * timer, 0.1 * Screen.height), "TIMER", style);
-		//instance.timerBarRect.width = TimerBarWidth * (((int)timer) / 200);
-		//instance.timerBarRect.height = 20;
+//		GUI.Label (new Rect (735, 12, 60, 20), timetext + timer.ToString(), style);
+//		GUI.Box (new Rect (10, 10, 0.001 * Screen.width * timer, 0.1 * Screen.height), "TIMER", style);
+//		instance.timerBarRect.width = TimerBarWidth * (((int)timer) / 200);
+//		instance.timerBarRect.height = 20;
 		
-		//instance.timerBarBackgroundRect.width = TimerBarWidth;
+//		instance.timerBarBackgroundRect.width = TimerBarWidth;
 		//instance.timerBarBackground.height = 20;
 		
-		//GUI.DrawTexture (timerBarRect, timerBar);
-		//GUI.DrawTexture (timerBarBackgroundRect, timerBarBackground);
+//		GUI.DrawTexture (timerBarRect, timerBar);
+//		GUI.DrawTexture (timerBarBackgroundRect, timerBarBackground);
 		
-		//GUI.Label (timerBarLabelRect, "TIME");
+//		GUI.Label (timerBarLabelRect, "TIME");
 		
-		//GUI.Box (new Rect (10, 10, 300, 20), new GUIContent(bgImage));
-		
+//		GUI.Box (new Rect (10, 10, 300, 20), new GUIContent(bgImage));
+
+//		if((time - timer)/time > 0.001f && timer > 0){	
+//			GUI.BeginGroup (new Rect (0, 0, Screen.width, Screen.height));
+//				GUI.Box (new Rect (10,10, timerBarLength, 20), "BACKGROUND");
+//		
+//				GUI.BeginGroup (new Rect (0, 0, Screen.width, Screen.height));
+//					GUI.Box ( new Rect (10,10, ((time-timer)/time) *timerBarLength, 20), "POWER");
+//				GUI.EndGroup ();
+//			GUI.EndGroup ();
+//		}
+		if((time - timer)/time > 0.001f && timer > 0 ){
+			pbbackground.transform.position = new Vector3(0, 1, -1);
+			pbbackground.transform.localScale = new Vector2(1, 0);
+			powerbar.transform.position = new Vector3((0.5f*(time-timer)/time)-0.55f, 1, 1);
+			powerbar.transform.localScale = new Vector2(1, 0);
+		}
+		else{
+			pbbackground.transform.position = new Vector3(0, 2, -1);
+			powerbar.transform.position = new Vector3(0, 2, 1);
+		}
 	}
 	
 }
