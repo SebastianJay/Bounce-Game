be19660c111759c5370102bc646688069bb7def8
Brightlad <bsb9hh@virginia.edu>
2015-04-13 18:59:11 -0400

    Bonus 2
    rough bonus level

---
+++ Bounce/Assets/Scripts/Environment/Player/Inventory.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+/// <summary>
+/// Contains an with a slot for each item type, the value in the 
+/// slot indicates the quantity held of that item
+/// </summary>
+public class Inventory {
+
+	public static int NUM_ITEMS =
+		System.Enum.GetNames(typeof(ItemType)).Length;
+
+	//public int[] items = new int[NUM_ITEMS];
+	public int[] items = new int[50];	//makes it easy to expand the enum w/o messing up save file
+
+	public bool HasItem(ItemType type)
+	{
+		return NumItem(type) > 0;
+	}
+
+	public int NumItem(ItemType type)
+	{
+		return items[(int)type];
+	}
+
+	public void AddItem(ItemType type)
+	{
+		ShiftItemCount(type, 1);
+	}
+
+	public void RemoveItem(ItemType type)
+	{
+		ShiftItemCount(type, -1);
+	}
+
+	public void ShiftItemCount(ItemType type, int num)
+	{
+		items[(int)type] += num;
+		if (items[(int)type] < 0)
+			items[(int)type] = 0;
+	}
+
+	public void SetItemCount(ItemType type, int num)
+	{
+		if (num >= 0)
+			items[(int)type] = num;
+	}
+
+	public List<int> ToList()
+	{
+		return new List<int>(items);
+	}
+	
+	public void Load(List<int> list)
+	{
+		items = list.ToArray();
+	}
+}
