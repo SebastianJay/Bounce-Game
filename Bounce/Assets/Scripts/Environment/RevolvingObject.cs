9b271f8388b8e4821f7bb8465eebac334c7f4cdf
Julian <jaykmcclinton@gmail.com>
2015-04-27 09:29:47 -0400

    final space test
    - added another belt to the test world
    - also tweaked Dynamic spawner script to limit spawning distance from
    player, basically to minimize # of game objects and potential lag

--- Bounce/Assets/Scripts/Environment/RevolvingObject.cs
+++ Bounce/Assets/Scripts/Environment/RevolvingObject.cs
@@ -5,10 +5,13 @@ public class RevolvingObject : MonoBehaviour {
 
 	public float rotationalVelocity = 1f;
 	public GameObject center;
+	float distance;
 
 	// Use this for initialization
 	void Start () {
-	
+		float dx = transform.position.x - center.transform.position.x;
+		float dy = transform.position.y - center.transform.position.y;
+		distance = Mathf.Sqrt (dx * dx + dy * dy);
 	}
 	
 	// Update is called once per frame
@@ -16,7 +19,6 @@ public class RevolvingObject : MonoBehaviour {
 		if (center != null) {
 						float dx = transform.position.x - center.transform.position.x;
 						float dy = transform.position.y - center.transform.position.y;
-						float distance = Mathf.Sqrt (dx * dx + dy * dy);
 						float theta = Mathf.Atan2 (dy, dx) + Time.deltaTime * rotationalVelocity * .5f;
 
 						float newX = center.transform.position.x + distance * Mathf.Cos (theta);
--- Bounce/Assets/Scripts/PickUp/DynamicSpawner.cs
+++ Bounce/Assets/Scripts/PickUp/DynamicSpawner.cs
@@ -12,25 +12,33 @@ public class DynamicSpawner : Spawner {
 
 	public override void DoSpawn() {
 		int index = Random.Range (0, objLst.Count);
-		Transform clone = Instantiate (objLst [index], transform.position, Quaternion.identity) as Transform;
-		if (clone.GetComponent<MovingPlatform>() != null)
-		{
-			float transVel = Random.Range(transVelMin, transVelMax);
-			float transMoveTime = moveDelta.magnitude / transVel;
-			clone.GetComponent<MovingPlatform>().useCurrentStartPosition = true;
-			clone.GetComponent<MovingPlatform>().moveTime = transMoveTime;
-			clone.GetComponent<MovingPlatform>().endPoint = moveDelta;
-			if (clone.GetComponent<SelfRemove>() != null)
-			{
-				clone.GetComponent<SelfRemove>().activeTime = transMoveTime;
-			}
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		float dx = transform.position.x - player.transform.position.x;
+		float dy = transform.position.y - player.transform.position.y;
+		float distance = Mathf.Sqrt (dx * dx + dy * dy);
 
-		}
-		if (clone.GetComponent<RotatingObject>() != null)
+		if(Mathf.Abs (distance) < 300f)
 		{
-			float rotVel = Random.Range(rotVelMin, rotVelMax);
-			clone.GetComponent<RotatingObject>().rotationalVelocity = rotVel;
-			clone.RotateAround(clone.transform.position, new Vector3(0f, 0f, 1f), Random.Range(0f, 360f));
+			Transform clone = Instantiate (objLst [index], transform.position, Quaternion.identity) as Transform;
+			if (clone.GetComponent<MovingPlatform>() != null)
+			{
+				float transVel = Random.Range(transVelMin, transVelMax);
+				float transMoveTime = moveDelta.magnitude / transVel;
+				clone.GetComponent<MovingPlatform>().useCurrentStartPosition = true;
+				clone.GetComponent<MovingPlatform>().moveTime = transMoveTime;
+				clone.GetComponent<MovingPlatform>().endPoint = moveDelta;
+				if (clone.GetComponent<SelfRemove>() != null)
+				{
+					clone.GetComponent<SelfRemove>().activeTime = transMoveTime;
+				}
+				
+			}
+			if (clone.GetComponent<RotatingObject>() != null)
+			{
+				float rotVel = Random.Range(rotVelMin, rotVelMax);
+				clone.GetComponent<RotatingObject>().rotationalVelocity = rotVel;
+				clone.RotateAround(clone.transform.position, new Vector3(0f, 0f, 1f), Random.Range(0f, 360f));
+			}
 		}
 	}
 }
